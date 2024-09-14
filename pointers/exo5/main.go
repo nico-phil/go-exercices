524fdb00034d14f2b49dff2a0fbf6e4770d46f4c
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-09-14 06:02:10 +0300

    add exo 5

--- pointers/exo5/main.go
+++ pointers/exo5/main.go
@@ -2,26 +2,41 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name string
-	age int
+
+func ModifyMap(m map[string]int){
+	for k, v := range m {
+		ptr := &v
+
+		*ptr = *ptr * 2
+
+		m[k] = *ptr
+	}
 }
 
-func Modify( p *Person){
-	p.age = 30
+
+func update(m map[string]int,  key string, v *int){
+		if _, ok := m[key]; !ok {
+			m[key] = *v
+		}
 }
 
-func(p *Person) Update(){
-	p.age = 150
-}
 
 func main(){
-	p := Person {
-		name: "Nico",
-		age: 22,
+	m := map[string]int{
+		"one": 1,
+		"two": 2,
+		"three": 3,
 	}
+	
+	v := 4
+	
+	fmt.Println("before", m)
 
-	p.Update()
 
-	fmt.Println(p)
+	update(m, "four", &v)
+
+	// ModifyMap(m)
+
+	fmt.Println("after", m)
 }
+
