30b385d1d13a8762936da70024c396bd1fb2f880
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-09-22 03:54:23 +0300

    chapter(context): add exo1.3

--- context/exo1.3/main.go
+++ context/exo1.3/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+
+func One(ctx context.Context){
+	time.Sleep(time.Millisecond * 500)
+	if ctx.Err() != nil {
+		fmt.Println("One: cancelled")
+		return
+	}
+	
+	fmt.Println("funcOne: running")
+
+	two(ctx)
+}
+
+func two(ctx context.Context){
+	time.Sleep(time.Second)
+	if ctx.Err() != nil {
+		fmt.Println("two: cancelled")
+		return
+	}
+	
+	fmt.Println("funcTwo: running")
+
+	three(ctx)
+}
+
+func three(ctx context.Context){
+	time.Sleep(time.Second * 2)
+	if ctx.Err() != nil {
+		fmt.Println("three: cancelled")
+		return
+	}
+	
+	fmt.Println("funThree: running")
+}
+
+
 
 func main(){
-	
+	ctx, cancel :=  context.WithCancel(context.Background())
+
+	go One(ctx)
+
+	time.Sleep(time.Second * 2)
+	cancel()
+
+	time.Sleep(time.Second * 2)
 }
